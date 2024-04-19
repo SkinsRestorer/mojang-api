c458d4cca7dd7f2e2a0f01ef4daf91c83c9bd568
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-04-19 17:29:59 +0200

    Make main method smaller

--- src/main/java/net/skinsrestorer/mojangapi/Main.java
+++ src/main/java/net/skinsrestorer/mojangapi/Main.java
@@ -2,7 +2,6 @@ package net.skinsrestorer.mojangapi;
 
 import com.linecorp.armeria.server.RedirectService;
 import com.linecorp.armeria.server.Server;
-import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.docs.DocService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,7 +9,21 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
   public static void main(String[] args) {
     try (var databaseManager = new DatabaseManager()) {
-      final Server server = newServer(Integer.parseInt(System.getenv("SERVER_PORT")), databaseManager);
+      final Server server =
+        Server.builder()
+          .http(Integer.parseInt(System.getenv("SERVER_PORT")))
+          .annotatedService("/mojang", new MojangAPIProxyService(databaseManager))
+          .service("/", new RedirectService("/docs"))
+          .serviceUnder("/docs",
+            DocService.builder()
+              .examplePaths(MojangAPIProxyService.class,
+                "nameToUUID",
+                "/mojang/uuid/Pistonmaster")
+              .examplePaths(MojangAPIProxyService.class,
+                "uuidToSkin",
+                "/mojang/skin/b1ae0778-4817-436c-96a3-a72c67cda060")
+              .build())
+          .build();
 
       server.closeOnJvmShutdown();
 
@@ -23,32 +36,5 @@ public class Main {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
-  }
-
-  /**
-   * Returns a new {@link Server} instance configured with annotated HTTP services.
-   *
-   * @param port the port that the server is to be bound to
-   */
-  @SuppressWarnings("SameParameterValue")
-  private static Server newServer(int port, DatabaseManager databaseManager) {
-    final ServerBuilder sb = Server.builder();
-    sb.http(port);
-    configureServices(sb, databaseManager);
-    return sb.build();
-  }
-
-  static void configureServices(ServerBuilder sb, DatabaseManager databaseManager) {
-    sb.annotatedService("/mojang", new MojangAPIProxyService(databaseManager))
-      .service("/", new RedirectService("/docs"))
-      .serviceUnder("/docs",
-        DocService.builder()
-          .examplePaths(MojangAPIProxyService.class,
-            "nameToUUID",
-            "/mojang/uuid/Pistonmaster")
-          .examplePaths(MojangAPIProxyService.class,
-            "uuidToSkin",
-            "/mojang/skin/b1ae0778-4817-436c-96a3-a72c67cda060")
-          .build());
   }
 }
