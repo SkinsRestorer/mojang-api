7638eb548e35fa1a2ed5a14165ba867ab7f65e19
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-19 16:44:56 +0200

    Resolve connection pool issues

--- src/main/java/net/skinsrestorer/mojangapi/DatabaseManager.java
+++ src/main/java/net/skinsrestorer/mojangapi/DatabaseManager.java
@@ -4,6 +4,7 @@ import io.r2dbc.postgresql.codec.PostgresqlObjectId;
 import io.r2dbc.spi.*;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Nullable;
@@ -15,6 +16,7 @@ import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import static io.r2dbc.postgresql.PostgresqlConnectionFactoryProvider.APPLICATION_NAME;
 import static io.r2dbc.spi.ConnectionFactoryOptions.*;
@@ -47,147 +49,152 @@ public class DatabaseManager implements AutoCloseable {
     scheduleCleanup();
   }
 
-  private Mono<? extends Connection> createConnection() {
-    return Mono.from(connectionFactory.create());
+  private <T> Mono<T> useConnectionMono(Function<Mono<? extends Connection>, ? extends Mono<? extends T>> consumer) {
+    return Mono.usingWhen(connectionFactory.create(), c -> consumer.apply(Mono.just(c)), Connection::close);
+  }
+
+  private <T> Flux<T> useConnectionFlux(Function<Mono<? extends Connection>, ? extends Flux<? extends T>> consumer) {
+    return Flux.usingWhen(connectionFactory.create(), c -> consumer.apply(Mono.just(c)), Connection::close);
   }
 
   public void createTables() {
-    var conn = createConnection();
-    conn.flatMapMany(it -> it.createStatement(
-          "CREATE TABLE IF NOT EXISTS uuid_cache ("
-            + "name VARCHAR(16) PRIMARY KEY,"
-            + "uuid VARCHAR(36),"
-            + "created_at TIMESTAMP NOT NULL"
-            + ")")
-        .execute())
+    useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+        "CREATE TABLE IF NOT EXISTS uuid_cache ("
+          + "name VARCHAR(16) PRIMARY KEY,"
+          + "uuid VARCHAR(36),"
+          + "created_at TIMESTAMP NOT NULL"
+          + ")")
+      .execute()))
       .blockLast();
 
-    conn.flatMapMany(it -> it.createStatement(
-          "CREATE TABLE IF NOT EXISTS skin_cache ("
-            + "uuid VARCHAR(36) PRIMARY KEY,"
-            + "value TEXT,"
-            + "signature TEXT,"
-            + "created_at TIMESTAMP NOT NULL"
-            + ")")
-        .execute())
+    useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+        "CREATE TABLE IF NOT EXISTS skin_cache ("
+          + "uuid VARCHAR(36) PRIMARY KEY,"
+          + "value TEXT,"
+          + "signature TEXT,"
+          + "created_at TIMESTAMP NOT NULL"
+          + ")")
+      .execute()))
       .blockLast();
 
     // Delete old rows if the table exceeds the maximum number of rows
-    conn.flatMapMany(it -> it.createStatement(
-          "CREATE OR REPLACE FUNCTION delete_old_uuid_cache_rows() RETURNS TRIGGER AS $$"
-            + "BEGIN"
-            + " IF (SELECT COUNT(*) FROM uuid_cache) > " + UUID_MAX_ROWS + " THEN"
-            + " DELETE FROM uuid_cache WHERE created_at = (SELECT MIN(created_at) FROM uuid_cache);"
-            + " END IF;"
-            + " RETURN NEW;"
-            + "END;"
-            + "$$ LANGUAGE plpgsql")
-        .execute())
+    useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+        "CREATE OR REPLACE FUNCTION delete_old_uuid_cache_rows() RETURNS TRIGGER AS $$"
+          + "BEGIN"
+          + " IF (SELECT COUNT(*) FROM uuid_cache) > " + UUID_MAX_ROWS + " THEN"
+          + " DELETE FROM uuid_cache WHERE created_at = (SELECT MIN(created_at) FROM uuid_cache);"
+          + " END IF;"
+          + " RETURN NEW;"
+          + "END;"
+          + "$$ LANGUAGE plpgsql")
+      .execute()))
       .blockLast();
 
     if (!triggerExists("delete_old_uuid_cache_rows_trigger")) {
-      conn.flatMapMany(it -> it.createStatement(
-            "CREATE TRIGGER delete_old_uuid_cache_rows_trigger"
-              + " AFTER INSERT ON uuid_cache"
-              + " EXECUTE FUNCTION delete_old_uuid_cache_rows()")
-          .execute())
+      useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+          "CREATE TRIGGER delete_old_uuid_cache_rows_trigger"
+            + " AFTER INSERT ON uuid_cache"
+            + " EXECUTE FUNCTION delete_old_uuid_cache_rows()")
+        .execute()))
         .blockLast();
     }
 
     // Delete old rows if the table exceeds the maximum number of rows
-    conn.flatMapMany(it -> it.createStatement(
-          "CREATE OR REPLACE FUNCTION delete_old_skin_cache_rows() RETURNS TRIGGER AS $$"
-            + "BEGIN"
-            + " IF (SELECT COUNT(*) FROM skin_cache) > " + SKIN_MAX_ROWS + " THEN"
-            + " DELETE FROM skin_cache WHERE created_at = (SELECT MIN(created_at) FROM skin_cache);"
-            + " END IF;"
-            + " RETURN NEW;"
-            + "END;"
-            + "$$ LANGUAGE plpgsql")
-        .execute())
+    useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+        "CREATE OR REPLACE FUNCTION delete_old_skin_cache_rows() RETURNS TRIGGER AS $$"
+          + "BEGIN"
+          + " IF (SELECT COUNT(*) FROM skin_cache) > " + SKIN_MAX_ROWS + " THEN"
+          + " DELETE FROM skin_cache WHERE created_at = (SELECT MIN(created_at) FROM skin_cache);"
+          + " END IF;"
+          + " RETURN NEW;"
+          + "END;"
+          + "$$ LANGUAGE plpgsql")
+      .execute()))
       .blockLast();
 
     if (!triggerExists("delete_old_skin_cache_rows_trigger")) {
-      conn.flatMapMany(it -> it.createStatement(
-            "CREATE TRIGGER delete_old_skin_cache_rows_trigger"
-              + " AFTER INSERT ON skin_cache"
-              + " EXECUTE FUNCTION delete_old_skin_cache_rows()")
-          .execute())
+      useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+          "CREATE TRIGGER delete_old_skin_cache_rows_trigger"
+            + " AFTER INSERT ON skin_cache"
+            + " EXECUTE FUNCTION delete_old_skin_cache_rows()")
+        .execute()))
         .blockLast();
     }
   }
 
   private boolean triggerExists(String triggerName) {
-    return Boolean.TRUE.equals(createConnection()
-      .flatMapMany(it -> it.createStatement("SELECT 1 FROM pg_trigger WHERE tgname = $1")
-        .bind("$1", triggerName)
-        .execute())
-      .map(it -> true)
-      .defaultIfEmpty(false)
-      .blockLast());
+    return Boolean.TRUE.equals(
+      useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+            "SELECT 1 FROM pg_trigger WHERE tgname = $1")
+          .bind("$1", triggerName)
+          .execute())
+        .map(it -> true)
+        .defaultIfEmpty(false))
+        .blockLast());
   }
 
   public void scheduleCleanup() {
     cleanupExecutor.scheduleWithFixedDelay(() -> {
-      createConnection()
-        .flatMapMany(it -> it.createStatement("DELETE FROM uuid_cache WHERE created_at < NOW() - INTERVAL '" + UUID_CACHE_DURATION.toHours() + "' HOUR")
+      useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+            "DELETE FROM uuid_cache WHERE created_at < NOW() - INTERVAL '" + UUID_CACHE_DURATION.toHours() + "' HOUR")
           .execute())
         .flatMap(Result::getRowsUpdated)
-        .doOnNext(it -> log.info("Cleaned up {} rows from uuid_cache", it))
+        .doOnNext(it -> log.info("Cleaned up {} rows from uuid_cache", it)))
         .subscribe();
 
-      createConnection()
-        .flatMapMany(it -> it.createStatement("DELETE FROM skin_cache WHERE created_at < NOW() - INTERVAL '" + SKIN_CACHE_DURATION.toHours() + "' HOUR")
+      useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+            "DELETE FROM skin_cache WHERE created_at < NOW() - INTERVAL '" + SKIN_CACHE_DURATION.toHours() + "' HOUR")
           .execute())
         .flatMap(Result::getRowsUpdated)
-        .doOnNext(it -> log.info("Cleaned up {} rows from skin_cache", it))
+        .doOnNext(it -> log.info("Cleaned up {} rows from skin_cache", it)))
         .subscribe();
     }, 0, 6, TimeUnit.HOURS);
   }
 
   public void putNameToUUID(String name, @Nullable UUID uuid, LocalDateTime createdAt) {
-    createConnection()
-      .flatMapMany(it -> it.createStatement("INSERT INTO uuid_cache (name, uuid, created_at) VALUES ($1, $2, $3) ON CONFLICT (name) DO UPDATE SET uuid = EXCLUDED.uuid")
+    useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+          "INSERT INTO uuid_cache (name, uuid, created_at) VALUES ($1, $2, $3) ON CONFLICT (name) DO UPDATE SET uuid = EXCLUDED.uuid")
         .bind("$1", name)
         .bind("$2", Parameters.in(PostgresqlObjectId.VARCHAR, uuid == null ? null : uuid.toString()))
         .bind("$3", createdAt)
         .execute())
       .flatMap(Result::getRowsUpdated)
-      .doOnNext(it -> log.debug("Inserted {} rows into uuid_cache", it))
+      .doOnNext(it -> log.debug("Inserted {} rows into uuid_cache", it)))
       .subscribe();
   }
 
   public Mono<DatabaseResult<UUID>> getNameToUUID(String name) {
-    return createConnection()
-      .flatMapMany(it -> it.createStatement("SELECT uuid, created_at FROM uuid_cache WHERE name = $1")
+    return useConnectionMono(conn -> conn.flatMapMany(it -> it.createStatement(
+          "SELECT uuid, created_at FROM uuid_cache WHERE name = $1")
         .bind("$1", name)
         .execute())
       .next()
       .flatMapMany(it -> it.map((row, meta) -> {
+        System.out.println("getNameToUUID map");
         var createdAt = Objects.requireNonNull(row.get("created_at", LocalDateTime.class));
         var uuid = row.get("uuid", String.class);
 
         return new DatabaseResult<>(createdAt.toEpochSecond(ZoneOffset.UTC), uuid == null ? null : UUID.fromString(uuid));
       }))
-      .next();
+      .next());
   }
 
   public void putUUIDToSkin(UUID uuid, @Nullable SkinProperty skinProperty, LocalDateTime createdAt) {
-    createConnection()
-      .flatMapMany(it -> it.createStatement("INSERT INTO skin_cache (uuid, value, signature, created_at) VALUES ($1, $2, $3, $4) ON CONFLICT (uuid) DO UPDATE SET value = EXCLUDED.value, signature = EXCLUDED.signature")
+    useConnectionFlux(conn -> conn.flatMapMany(it -> it.createStatement(
+          "INSERT INTO skin_cache (uuid, value, signature, created_at) VALUES ($1, $2, $3, $4) ON CONFLICT (uuid) DO UPDATE SET value = EXCLUDED.value, signature = EXCLUDED.signature")
         .bind("$1", uuid.toString())
         .bind("$2", Parameters.in(PostgresqlObjectId.TEXT, skinProperty == null ? null : skinProperty.value()))
         .bind("$3", Parameters.in(PostgresqlObjectId.TEXT, skinProperty == null ? null : skinProperty.signature()))
         .bind("$4", createdAt)
         .execute())
       .flatMap(Result::getRowsUpdated)
-      .doOnNext(it -> log.debug("Inserted {} rows into skin_cache", it))
+      .doOnNext(it -> log.debug("Inserted {} rows into skin_cache", it)))
       .subscribe();
   }
 
   public Mono<DatabaseResult<SkinProperty>> getUUIDToSkin(UUID uuid) {
-    return createConnection()
-      .flatMapMany(it -> it.createStatement("SELECT value, signature, created_at FROM skin_cache WHERE uuid = $1")
+    return useConnectionMono(conn -> conn.flatMapMany(it -> it.createStatement(
+          "SELECT value, signature, created_at FROM skin_cache WHERE uuid = $1")
         .bind("$1", uuid.toString())
         .execute())
       .next()
@@ -198,7 +205,7 @@ public class DatabaseManager implements AutoCloseable {
 
         return new DatabaseResult<>(createdAt.toEpochSecond(ZoneOffset.UTC), value == null || signature == null ? null : new SkinProperty(value, signature));
       }))
-      .next();
+      .next());
   }
 
   @Override
